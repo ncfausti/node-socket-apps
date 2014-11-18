9a8fc9cb7df693498bbc20fbafcb61f8f65f2a60
Nick Fausti <ncfausti@gmail.com>
2014-11-18 00:45:54 -0500

    fully working real-time chat application

--- chat/index.js
+++ chat/index.js
@@ -11,11 +11,14 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 	console.log('a user connected');
+	
 	socket.on('disconnect', function() {
 		console.log('user disconnected');
 	});
+
 	socket.on('chat message', function(msg){
 		console.log('message ' + msg);
+		io.emit('chat message', msg);
 	});
 });
 
