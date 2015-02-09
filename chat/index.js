1661b1b0e1d1e4342375c491676c311cbd635c12
Nick Fausti <ncfausti@gmail.com>
2015-02-08 21:06:20 -0500

    updated chat client

--- chat/index.js
+++ chat/index.js
@@ -4,9 +4,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/chat', function(req, res){
 	//res.send('<h1>Hello world</h1');  // just sends text to the browser
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/chat.html');
 });
 
 io.on('connection', function(socket){
